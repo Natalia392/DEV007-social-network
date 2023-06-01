24d7877a60a322a85242b71d7a1da3e552a241db
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-06-01 10:25:23 -0400

    añade secciónde register

---
+++ src/components/Register.js
@@ -0,0 +1,22 @@
+export const Register = () => {
+  const RegisterDiv = document.createElement('div');
+  const nameRegister = document.createElement('p');
+  const inputNameRegister = document.createElement('input');
+  const emailRegister = document.createElement('p');
+  const inputEmailRegister = document.createElement('input');
+  const passwordRegister = document.createElement('p');
+  const inputCreatePassword = document.createElement('input');
+  const buttonCreateAcount = document.createElement('button');
+
+  nameRegister.textContent = 'Nombre';
+  emailRegister.textContent = 'Correo';
+  passwordRegister.textContent = 'Constraseña';
+  buttonCreateAcount.textContent = 'Crear cuenta';
+
+  RegisterDiv.appendChild(nameRegister);
+  RegisterDiv.appendChild(inputNameRegister);
+  RegisterDiv.appendChild(emailRegister);
+  RegisterDiv.appendChild(inputEmailRegister);
+  RegisterDiv.appendChild(inputCreatePassword);
+  RegisterDiv.appendChild(buttonCreateAcount);
+};
