b9d669c3fb55a4681943b68ffd07e5139e9ce445
paolandre <paolandre38@gmail.com>
2023-06-16 06:11:32 -0500

    Cambios en el CSS de la ventana del wall

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -2,21 +2,49 @@ import { createPost, getPosts } from '../lib';
 import { showMessage } from './modal';
 
 export const Wall = (onNavigate) => {
+  // Crea el div donde están todos los elementos del wall
   const wallDiv = document.createElement('main');
   wallDiv.className = 'wall-main';
+
+  // Crea el header div
+  const headerWall = document.createElement('div');
+  headerWall.className = 'header-wall';
+
+  // Crea el logo
+  const logoWall = document.createElement('img');
+  logoWall.src = './assets/images/Logo-Comunidad.png';
+  logoWall.className = 'logo-wall';
+  headerWall.appendChild(logoWall);
+
+  // Crea el titulo de la comunidad
+  const name = document.createElement('p');
+  name.textContent = 'La Comunidad del Libro';
+  name.className = 'comunidad-libro-wall';
+  headerWall.appendChild(name);
+
+  // Crea el a de cerrar sesión
+  const logoutButton = document.createElement('a');
+  logoutButton.textContent = 'Cerrar sesión';
+  logoutButton.className = 'cerrar-sesion';
+  headerWall.appendChild(logoutButton);
+
+  // Apendiza el header al wallDiv
+  wallDiv.appendChild(headerWall);
+
+  // Sección de los posts
   const section = document.createElement('section');
+  section.className = 'posts-section';
   section.innerHTML = `
     <div class="new-post-container" id="new-post-container">
-      <h2>Pronto podrás postear aquí</h2>
-      <textarea class="new-post-text"></textarea><br>
-      <button id="post-button" class="post-button">Publicar</button>
+      <input class="new-post-text" placeholder="Escribe aquí lo que quieras compartir sobre libros que hayas leído recientemente"></input><br>
+      <button id="post-button" class="post-button">Publica tu post</button>
     </div>
-    <div class="posts">
       <div class="all-posts">
-      </div>
+    </div>
+    <div>
       <button id="go-home" class="go-home">Home</button>
     </div>
-  `;
+`;
 
   section.querySelector('#post-button').addEventListener('click', async () => {
     const textAreaContent = section.querySelector('.new-post-text').value;
@@ -35,7 +63,7 @@ export const Wall = (onNavigate) => {
         // Apendizar el nuevo div al contenedor de posts
         const allPostsContainer = section.querySelector('.all-posts');
         allPostsContainer.appendChild(postDiv);
-        section.querySelector('.new-post-text').value = '';
+        section.querySelector('.new-post-text').value = ''; // Limpia el input después de publicado el mensaje
       }
     } catch (error) {
       console.error(error);
